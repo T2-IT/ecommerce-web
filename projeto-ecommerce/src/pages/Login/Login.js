40c74467f3bbca95aae3593ef9b4b9b29e99cd66
Arthurfdo <arthurfdo123@gmail.com>
2021-06-23 19:54:54 -0300

    login

--- projeto-ecommerce/src/pages/Login/Login.js
+++ projeto-ecommerce/src/pages/Login/Login.js
@@ -1,7 +1,32 @@
-function Login () {
- return (
-     <div>Login ne</div>
- )
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
+//import logo from '../../assets/logoT2.svg';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('CLICOU')
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder="***" value={password} onChange={(e) => setPassword(e.target.value) } />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
