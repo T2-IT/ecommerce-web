9ef18389975a7bab50cc3e6e3f00b17b93338026
hmZ <duduhammes@hotmail.com>
2021-06-23 09:53:07 -0300

    Vasco einh

--- projeto-ecommerce/src/navbar/pageNavBar/NavBar.js
+++ projeto-ecommerce/src/navbar/pageNavBar/NavBar.js
@@ -1,21 +1,21 @@
-import {Container, LoginButton, LogoT2} from './Styles';
+import {Nav,LoginButton,LogoT2,MenuButton,HomeButton} from './Styles';
 import {Link} from 'react-router-dom';
 import logo from '../../../src/logoT2.svg';
+import menuIcon from '../../../src/menuIcon.svg'
 
 function NavBar() {
  return (
-    <Container>
-       <div>
+    <Nav>
        <Link to={"/login"}>
           <LoginButton title="login">Login</LoginButton>
        </Link>
-       </div>
-       <div>
        <Link to="/">
+          <HomeButton title="Home">
           <LogoT2 src={logo} alt="logoT2"/>
+          </HomeButton>
        </Link>
-       </div>
-    </Container>
+       <MenuButton><img src={menuIcon} alt="menu"/></MenuButton>
+    </Nav>
  );
  }
 
--- projeto-ecommerce/src/navbar/pageNavBar/Styles.js
+++ projeto-ecommerce/src/navbar/pageNavBar/Styles.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    display: flex;
+export const Nav = styled.div`
+    padding-left: 0.25rem;
+    padding-right: 0;
+    list-style: none;
     position: -webkit-sticky;
     position: sticky;
     top: 0;
+    width: 100%;
+    margin-right: 0.15rem;
     padding-top: 0.10rem;
     padding-bottom: 0.10rem;
     padding-right: 0.10rem;
-    padding-left: 0.10rem;
     align-content: center;
     flex-wrap: wrap;
-    align-items: center;
+    align-content: center;
     justify-content: space-between;
     background-color: black;
-    width: 100%;
     position: -webkit-sticky;
     box-shadow: 2px 2px 5px rgb(0, 0, 0);
 `; 
@@ -45,4 +47,55 @@ export const LoginButton = styled.button `
 export const LogoT2 = styled.img`
     height: 1.24rem;
     width: 1.24rem;
+    position: relative;
+`;
+
+export const HomeButton = styled.button`
+    width: 94%;
+    display: inline-block;
+    font-weight: 400;
+    line-height: 1.5;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 0.7rem;
+    border-radius: 0.25rem;
+`;
+
+export const MenuButton = styled.button `
+    display: inline-block;
+    font-weight: 400;
+    line-height: 1.5;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 0.7rem;
+    border-radius: 0.25rem;
+    
+    &:hover{
+        box-shadow: 0 3px 5px 0 rgba(255, 255, 255, 0.2),
+        0 3px 5px 0 rgba(255, 255, 255, 0.19);
+    }
+`;
+
+export const MenuIcon = styled.img`
+    height: 1.34rem;
+    width: 1.34rem;
+    position: relative;
 `;
