125766da18001347958e12f5afa7bf58f76a1724
jgreis <jgreis.dev@protonmail.com>
2021-06-24 19:41:56 -0300

    Cadastrinho manhoso

--- projeto-ecommerce/src/App.js
+++ projeto-ecommerce/src/App.js
@@ -4,20 +4,22 @@ import NavBar from "./components/navbar/NavBar";
 import Conta from "./pages/Conta/Conta";
 import GLobalStyle from "./styles/global"
 import Login from "./pages/Login/Login";
-import './App.css';
 import Footer from "./components/Footer/Footer";
+import Cadastro from "./pages/Cadastro/Cadastro.js";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar/>
+      <NavBar />
       <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/conta" component={Conta}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/conta" component={Conta} />
+        <Route path="/registro" component={Cadastro} />
       </Switch>
-      <Footer/>
-      <GLobalStyle/>
+      <Footer />
+      <GLobalStyle />
     </BrowserRouter>
   );
 }
